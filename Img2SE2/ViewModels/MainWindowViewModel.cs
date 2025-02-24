54e329608dd8e3dfbf47a5f57779b5f5de86b567
Arthur <arthurvianasouza@yahoo.com.br>
2025-02-24 18:48:06 -0300

    Support for Multi-Frame images
    Added Support to Multi-Frame Images

--- Img2SE2/ViewModels/MainWindowViewModel.cs
+++ Img2SE2/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Input;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform.Storage;
@@ -30,15 +29,15 @@ public partial class MainWindowViewModel : ViewModelBase
 
     public Bitmap? SourceImage { get; set; }
 
+    public bool HasMultiFrames { get; set; }
+
     IStorageFile? File
     {
         get => _file;
         set
         {
             _file = value;
-            ConvertImageCommand.IsEnabled = File != null;
-            ConvertImageHeightMapCommand.IsEnabled = File != null;
-            UpdatePreview();
+            OnImageSelected();
         }
     }
 
@@ -52,12 +51,12 @@ public partial class MainWindowViewModel : ViewModelBase
         set
         {
             _tasks = value;
-            Dispatcher.UIThread.Post(() => OnPropertyChanged(nameof(Working))); 
+            Dispatcher.UIThread.Post(() => OnPropertyChanged(nameof(Working)));
         }
     }
 
     private StringBuilder _message = new();
-    
+
     public string Message => _message.ToString();
     public BlockSize[] BlockSizes { get; } = [BlockSize.Detailing, BlockSize.Small, BlockSize.Large];
     public BlockSize SelectedSize { get; set; } = BlockSize.Detailing;
@@ -83,12 +82,16 @@ public partial class MainWindowViewModel : ViewModelBase
                     "*.jpg",
                     "*.jpeg",
                     "*.bmp",
+                    "*.gif",
+                    "*.tif",
+                    "*.tiff"
                 },
                 MimeTypes = new[]
                 {
                     "image/png",
                     "image/jpeg",
                     "image/bmp",
+                    "image/tiff"
                 },
                 AppleUniformTypeIdentifiers = new[]
                 {
@@ -98,14 +101,61 @@ public partial class MainWindowViewModel : ViewModelBase
         };
     }
 
+    private async void OnImageSelected()
+    {
+        Tasks++;
+
+        try
+        {
+            ConvertImageCommand.IsEnabled = File != null;
+
+            if (File?.Path.AbsolutePath is { } path)
+            {
+                var info = await Image.IdentifyAsync(path);
+                HasMultiFrames = info.FrameMetadataCollection.Count > 1;
+                ConvertImageHeightMapCommand.IsEnabled = !HasMultiFrames;
+                if (HasMultiFrames)
+                {
+                    WriteMessage("Multi-Frame Image detected, using 3D image converter");
+                    WriteMessage("Height Map is not supported using this configuration");
+                }
+            }
+            else
+            {
+                HasMultiFrames = false;
+            }
+
+            UpdatePreview();
+        }
+        finally
+        {
+            Tasks--;
+        }
+    }
+
     public async void UpdatePreview()
     {
         try
         {
             using var task = _file?.OpenReadAsync();
-            if (task != null && await task is {} stream)
+            if (task != null && await task is { } stream)
             {
-                SourceImage = new Bitmap(stream);
+                if (HasMultiFrames)
+                {
+                    using (var image = await Image.LoadAsync(await task))
+                    {
+                        var firstFrame = image.Frames.CloneFrame(1);
+                        var frameStream = new MemoryStream();
+                        await firstFrame.SaveAsBmpAsync(frameStream);
+                        frameStream.Position = 0;
+                        SourceImage = new Bitmap(frameStream);
+                    }
+                }
+                else
+                {
+                    SourceImage = new Bitmap(stream);
+                }
+
                 OnPropertyChanged(nameof(SourceImage));
             }
         }
@@ -152,21 +202,25 @@ public partial class MainWindowViewModel : ViewModelBase
         {
             Tasks++;
 
-            var colorData = await ConvertImageTo2DArray(_file);
+            var stream = await _file.OpenReadAsync();
+            using Image<Rgba32> image = Image.Load<Rgba32>(stream);
+            var colorData = ConvertImageTo2DArray(image);
 
             Rgba32[,]? heightMap = null;
             if (await PickImageFileAsync("Pick a Height Map") is { } mask)
             {
-                heightMap = await ConvertImageTo2DArray(mask, colorData.GetLength(1), colorData.GetLength(0));
+                var maskStream = await mask.OpenReadAsync();
+                using Image<Rgba32> maskImage = Image.Load<Rgba32>(maskStream);
+                heightMap = ConvertImageTo2DArray(maskImage, colorData.GetLength(1), colorData.GetLength(0));
             }
 
             var path = GetNewBlueprintFilePath(File?.Name);
             await using var textFile = System.IO.File.CreateText(path);
-            
 
-            foreach (var line in ConvertArrayToBlocks(colorData, SelectedSize, heightMap))
+
+            foreach (var line in Convert2DArrayToBlocks(colorData, SelectedSize, heightMap))
                 await textFile.WriteLineAsync(line);
-            
+
             WriteMessage("Blueprint generated successfully");
             WriteMessage($"Path: {path}");
         }
@@ -179,7 +233,7 @@ public partial class MainWindowViewModel : ViewModelBase
             Tasks--;
         }
     }
-    
+
     public async void ConvertImage()
     {
         if (_file == null)
@@ -188,16 +242,30 @@ public partial class MainWindowViewModel : ViewModelBase
         try
         {
             Tasks++;
+            var stream = await _file.OpenReadAsync();
+            using Image<Rgba32> image = Image.Load<Rgba32>(stream);
 
-            var colorData = await ConvertImageTo2DArray(_file);
-            
             var path = GetNewBlueprintFilePath(File?.Name);
-            await using var textFile = System.IO.File.CreateText(path);
-            
 
-            foreach (var line in ConvertArrayToBlocks(colorData, SelectedSize))
-                await textFile.WriteLineAsync(line);
-            
+            if (image.Frames.Count > 1)
+            {
+                var colorData = ConvertImageTo3DArray(image);
+
+                await using var textFile = System.IO.File.CreateText(path);
+
+                foreach (var line in Convert3DArrayToBlocks(colorData, SelectedSize))
+                    await textFile.WriteLineAsync(line);
+            }
+            else
+            {
+                var colorData = ConvertImageTo2DArray(image);
+
+                await using var textFile = System.IO.File.CreateText(path);
+
+                foreach (var line in Convert2DArrayToBlocks(colorData, SelectedSize))
+                    await textFile.WriteLineAsync(line);
+            }
+
             WriteMessage("Blueprint generated successfully");
             WriteMessage($"Path: {path}");
         }
@@ -211,11 +279,8 @@ public partial class MainWindowViewModel : ViewModelBase
         }
     }
 
-    static async Task<Rgba32[,]> ConvertImageTo2DArray(IStorageFile imageFile, int? width = null, int? height = null)
+    static Rgba32[,] ConvertImageTo2DArray(Image<Rgba32> image, int? width = null, int? height = null)
     {
-        var stream = await imageFile.OpenReadAsync();
-        using Image<Rgba32> image = Image.Load<Rgba32>(stream);
-
         width ??= image.Width;
         height ??= image.Height;
 
@@ -232,7 +297,26 @@ public partial class MainWindowViewModel : ViewModelBase
         return colors;
     }
 
-    static string[] ConvertArrayToBlocks(Rgba32[,] colors, BlockSize selectedBlock, Rgba32[,]? heightMap = null)
+    static Rgba32[,,] ConvertImageTo3DArray(Image<Rgba32> image, int? width = null, int? height = null)
+    {
+        width ??= image.Width;
+        height ??= image.Height;
+
+        if (image.Width != width || image.Height != height)
+            image.Mutate(x => x.Resize((int)width, (int)height));
+
+
+        Rgba32[,,] colors = new Rgba32[image.Height, image.Width, image.Frames.Count];
+
+        for (int z = 0; z < image.Frames.Count; z++)
+        for (int y = 0; y < image.Height; y++)
+        for (int x = 0; x < image.Width; x++)
+            colors[y, x, z] = image.Frames[z][x, y];
+
+        return colors;
+    }
+
+    static string[] Convert2DArrayToBlocks(Rgba32[,] colors, BlockSize selectedBlock, Rgba32[,]? heightMap = null)
     {
         var array = new string[colors.Length];
 
@@ -241,15 +325,38 @@ public partial class MainWindowViewModel : ViewModelBase
         {
             var color = new ColorHSV(colors[y, x]);
             var z = heightMap != null && new ColorHSV(heightMap[y, x]).Value > 0.5 ? 1 : 0;
-            array[y * colors.GetLength(1) + x] = 
-                String.Format("{0}|{1}|{2}|{3}|{4}|{5}|{6}|0|4|1|", 
-                selectedBlock.GUID, 
-                x * selectedBlock.Size,
-                y * selectedBlock.Size, 
-                z * selectedBlock.Size, 
-                color.Hue, 
-                color.Saturation, 
-                color.Value);
+            array[y * colors.GetLength(1) + x] =
+                String.Format("{0}|{1}|{2}|{3}|{4}|{5}|{6}|0|4|1|",
+                    selectedBlock.GUID,
+                    x * selectedBlock.Size,
+                    y * selectedBlock.Size,
+                    z * selectedBlock.Size,
+                    color.Hue,
+                    color.Saturation,
+                    color.Value);
+        }
+
+        return array;
+    }
+
+    static string[] Convert3DArrayToBlocks(Rgba32[,,] colors, BlockSize selectedBlock)
+    {
+        var array = new string[colors.Length];
+
+        for (int y = 0; y < colors.GetLength(0); y++)
+        for (int x = 0; x < colors.GetLength(1); x++)
+        for (int z = 0; z < colors.GetLength(2); z++)
+        {
+            var color = new ColorHSV(colors[y, x, z]);
+            array[y * colors.GetLength(1) + x] =
+                String.Format("{0}|{1}|{2}|{3}|{4}|{5}|{6}|0|4|1|",
+                    selectedBlock.GUID,
+                    x * selectedBlock.Size,
+                    y * selectedBlock.Size,
+                    z * selectedBlock.Size,
+                    color.Hue,
+                    color.Saturation,
+                    color.Value);
         }
 
         return array;
